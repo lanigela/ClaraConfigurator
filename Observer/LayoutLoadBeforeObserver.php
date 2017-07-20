91ba4b2e27ca8881a0b35c69a5843dc34d6d82f4
Daniel <lanigela@gmail.com>
2017-07-20 14:23:53 -0400

    add log

--- Observer/LayoutLoadBeforeObserver.php
+++ Observer/LayoutLoadBeforeObserver.php
@@ -8,16 +8,20 @@ namespace Exocortex\ClaraConfigurator\Observer;
 use Magento\Framework\Event\Observer as EventObserver;
 use Magento\Framework\Event\ObserverInterface;
 use Magento\Catalog\Api\ProductRepositoryInterface as ProductRepositoryInterface;
+use Psr\Log\LoggerInterface as LoggerInterface;
 
 class LayoutLoadBeforeObserver implements ObserverInterface
 {
     private $_productRepository;
+    private $_logger;
     /**
      * @param \Magento\Catalog\Api\ProductRepositoryInterface $productRepository
      */
-    public function __construct(ProductRepositoryInterface $productRepository)
+    public function __construct(ProductRepositoryInterface $productRepository,
+                                LoggerInterface $logger)
     {
         $this->_productRepository = $productRepository;
+        $this->_logger = logger;
     }
 
     /**
@@ -28,13 +32,16 @@ class LayoutLoadBeforeObserver implements ObserverInterface
      */
     public function execute(\Magento\Framework\Event\Observer $observer)
     {
+        _logger->debug("LayoutLoadBeforeObserver executing");
         // apply to only bundle product for now
         if ($observer->getData('full_action_name')=='catalog_product_view_type_bundle') {
+            _logger->debug("Matching action name succeed");
             /* @var $product \Magento\Catalog\Model\Product */
             $product = $observer->getEvent()->getProduct();
             if ($product) {
                 $attr = $product->getData('claraUUID');
                 if($attr && !strcmp($attr, '')) {
+                    _logger.debug("Adding handle to layout");
                     $layout = $observer->getData('layout');
                     $layout->getUpdate()->addHandle('catalog_product_view_type_bundle_clara');
                 }
