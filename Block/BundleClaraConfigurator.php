78743b0ae13e65a51ca3f89139f8ff42c75d2fa4
Daniel <lanigela@gmail.com>
2017-07-28 12:39:17 -0400

    use block in module

---
+++ Block/BundleClaraConfigurator.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Copyright © Exocortex, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+/**
+ * Bundle product threekit configurator (Clara)
+ *
+ * @author      Daniel@Exocortex.com
+ */
+namespace Exocortex;
+
+class BundleClaraConfigurator
+{
+  /**
+  * @param \Magento\Bundle\Block\Catalog\Product\View\Type\Bundle
+  */
+  private $bundle;
+
+  /**
+  * @param \Magento\Catalog\Block\Product\View
+  */
+  private $view;
+
+  /**
+  * @param \Magento\Bundle\Block\Catalog\Product\View\Type\Bundle
+  * @param \Magento\Catalog\Block\Product\View
+  */
+  public function __construct(
+    \Magento\Bundle\Block\Catalog\Product\View\Type\Bundle $bundle,
+    \Magento\Catalog\Block\Product\View $view,
+    array $data = [])
+  {
+    $this->bundle = $bundle;
+    $this->view = $view;
+  }
+
+  public function getJsonConfig()
+  {
+    return $this->bundle->getJsonConfig();
+  }
+
+  public function getSubmitUrl($product, $additional = [])
+  {
+    return $this->view->getSubmitUrl($product, $additional);
+  }
+}
